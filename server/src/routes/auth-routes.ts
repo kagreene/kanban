e9071710816ca07c203664fd4283ff5efbde4212
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-15 11:01:06 -0600

    completed code in auth-routes file

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,25 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  //get username and password from request body
+  const { username, password } = req.body;
+  //check if user exists in database
+  const user = await User.findOne({ where: { username } });
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  //check if password is valid
+  const passwordValid = await bcrypt.compare(password, user.password);
+  if (!passwordValid) {
+    return res.status(400).json({ message: 'Invalid password' });
+  }
+
+  // get secret key from env variables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  // create a JWT token
+  const token = jwt.sign({ username}, secretKey, { expiresIn: '1h' });
+  // return the token
+  return res.json({ token });
 };
 
 const router = Router();
